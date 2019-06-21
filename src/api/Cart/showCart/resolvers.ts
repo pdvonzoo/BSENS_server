e00ae54dc2b561688a7dab7cedba88c227293d18
pdvonzoo <a01088570194@gmail.com>
2019-06-21 22:24:04 +0900

    Fix: show cart 반환값 수정

--- src/api/Cart/showCart/resolvers.ts
+++ src/api/Cart/showCart/resolvers.ts
@@ -6,13 +6,13 @@ const resolvers: ResolverMap = {
   Query: {
     showCart: async (_, __, { request }: any) => {
       const { products }: any = await Order.findOne({ userid: request.userid });
-      const cartItems = [];
+      let cartItems: any = [];
       if (products) {
         const cartList = products.map(async (product: any) =>
           Product.find({ _id: product.productid })
         );
         for await (const product of cartList) {
-          cartItems.push(product);
+          cartItems = [...cartItems, ...product];
         }
       }
       return cartItems;
