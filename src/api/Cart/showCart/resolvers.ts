4de3add75c103356ecfbf001ef3b401ad15b66fd
pdvonzoo <a01088570194@gmail.com>
2019-06-21 22:15:28 +0900

    Feat: show cart 비동기 로직 처리

--- src/api/Cart/showCart/resolvers.ts
+++ src/api/Cart/showCart/resolvers.ts
@@ -1,11 +1,21 @@
 import { ResolverMap } from "../../../Utils/gqlUtils";
 import { Order } from "../../../Model/Order";
+import { Product } from "../../../Model/Product";
 
 const resolvers: ResolverMap = {
   Query: {
     showCart: async (_, __, { request }: any) => {
-      await Order.findOne({ userid: request.user.id });
-      return true;
+      const { products }: any = await Order.findOne({ userid: request.userid });
+      const cartItems = [];
+      if (products) {
+        const cartList = products.map(async (product: any) =>
+          Product.find({ _id: product.productid })
+        );
+        for await (const product of cartList) {
+          cartItems.push(product);
+        }
+      }
+      return cartItems;
     }
   }
 };
